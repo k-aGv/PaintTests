693e5b94d1eb66bde8fc65491ad6865dbaf48b8a
Dimitris Katikaridis <dkatikaridis@gmail.com>
2016-05-10 11:42:10 -0700

    Finally !! Read description
    Finally found a way to delete a specific area of panel.This will help us
    delete/refresh only the moving object from panel.that means we no longer
    need Refreshgrid() function.We need 2 other protected refresh functions
    which will refresh the moved AGV and it's AOE

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        bool status = false;
+        bool status2 = false;
+        Graphics g,gp;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            status2 = false;
+            panel1.Refresh();
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (status)
+            {
+                Pen p =new Pen( Color.Black);
+                g = e.Graphics;
+                g.DrawRectangle(p, 10, 10, 20, 20);
+               // g.Dispose();
+            }
+            
+            if (status2)
+            {
+                Pen p2 = new Pen(Color.Red);
+                gp = e.Graphics;
+                gp.DrawRectangle(p2, 20, 20, 40, 40);
+                //gp.Dispose();
+            }
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            status = true;
+            panel1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            status = false;    
+            panel1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            status2 = true;
+            panel1.Refresh();
+        }
+
+     
+    }
+}
